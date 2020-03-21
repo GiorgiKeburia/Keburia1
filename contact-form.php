0cdcfd99b886df1732d30fd5123188b56039103d
GiorgiKeburia <57075219+GiorgiKeburia@users.noreply.github.com>
2020-03-21 16:23:23 +0400

    Add files via upload

--- contact-form.php
+++ contact-form.php
@@ -1,17 +1,26 @@
 <?php
-    $name=$_POST['name'];
-    $visitor_email=$_POST['E-mail'];
-    $message=$_POST['message'];
+    $name = $_POST['name'];
+    $visitor_email = $_POST['email'];
+    $message = $_POST['message'];
 
-    $email_from='Keburia.ge';
-    $email_subject="New Form Submition";
-    $email_body="User Name: $name.\n".  
+    $email_from = 'giorgikeburia1@gmail.com';
+
+    $email_subject = "New Form Submition";
+
+    $email_body = "User Name: $name.\n".  
                     "User Email: $visitor_email.\n".
                         "User Message: $message.\n";
 
-    $to="qeburia2000@gmail.com";
-    $headers="From: $email_from \r\n";
-    $headers .="Reply-To: $visitor_email \r\n";
+
+    $to = "qeburia2000@gmail.com"; 
+
+    $headers = "From: $email_from \r\n";
+
+    $headers .= "Reply-To: $visitor_email \r\n";
+
     mail($to,$email_subject,$email_body,$headers);
+
     header("Location: index.html");
+
+
 ?>
